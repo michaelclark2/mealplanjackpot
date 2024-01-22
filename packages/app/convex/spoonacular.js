a14b4e122dadede2fdf53719abf6e2464c2ee1ee
Michael Clark <michael.clark1992@gmail.com>
2024-01-22 09:40:37 -0600

    added default params to complexSearch

--- packages/app/convex/spoonacular.js
+++ packages/app/convex/spoonacular.js
@@ -3,9 +3,16 @@ import { internalAction } from './_generated/server'
 export const complexSearch = internalAction({
   args: {},
   handler: async () => {
-    const recipes = await fetch(
-      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.SPOONACULAR_APIKEY}`,
-    )
+    const baseURL = new URL('https://api.spoonacular.com/recipes/complexSearch')
+    const defaultParams = {
+      apiKey: process.env.SPOONACULAR_APIKEY,
+      addRecipeInformation: 'true',
+      sort: 'random',
+      type: 'main course',
+      number: '5',
+    }
+    const params = new URLSearchParams(defaultParams)
+    const recipes = await fetch(baseURL + '?' + params)
     return await recipes.json()
   },
 })
