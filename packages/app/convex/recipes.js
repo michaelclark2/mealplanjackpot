3f84b6661a2e62be7dd326b8ff13f74bd0c1fff6
Michael Clark <michael.clark1992@gmail.com>
2024-01-26 11:50:01 -0600

    added number arg

--- packages/app/convex/recipes.js
+++ packages/app/convex/recipes.js
@@ -2,7 +2,10 @@ import { httpAction } from './_generated/server'
 import { internal } from './_generated/api'
 
 export const getRandom = httpAction(async (ctx, request) => {
-  const recipes = await ctx.runAction(internal.spoonacular.complexSearch)
+  const number = new URL(request.url).searchParams.get('number')
+  const recipes = await ctx.runAction(internal.spoonacular.complexSearch, {
+    number,
+  })
   return new Response(JSON.stringify(recipes['results']), {
     status: 200,
     headers: new Headers({
--- packages/app/convex/spoonacular.js
+++ packages/app/convex/spoonacular.js
@@ -1,15 +1,21 @@
+import { ConvexError, v } from 'convex/values'
 import { internalAction } from './_generated/server'
 
 export const complexSearch = internalAction({
-  args: {},
-  handler: async () => {
+  args: {
+    number: v.string(),
+  },
+  handler: async (ctx, args) => {
+    console.log(args.number)
+    if (Number(args.number) <= 0 || Number(args.number) >= 8)
+      throw new ConvexError('Query parameters are incorrect')
     const baseURL = new URL('https://api.spoonacular.com/recipes/complexSearch')
     const defaultParams = {
       apiKey: process.env.SPOONACULAR_APIKEY,
       addRecipeInformation: 'true',
       sort: 'random',
       type: 'main course',
-      number: '5',
+      number: args.number,
     }
     const params = new URLSearchParams(defaultParams)
     const recipes = await fetch(baseURL + '?' + params)
