58c270a9b00af2bc99d23c222181329fe280d84b
Michael Clark <michael.clark1992@gmail.com>
2024-01-30 14:04:50 -0600

    refactored shared quota logic

--- packages/app/convex/recipes.js
+++ packages/app/convex/recipes.js
@@ -3,76 +3,25 @@ import { internal } from './_generated/api'
 
 export const getRandom = httpAction(async (ctx, request) => {
   const identity = await ctx.auth.getUserIdentity()
+  const identifier = identity
+    ? identity.email
+    : request.headers.get('x-forwarded-for') // ip address
+
   let results = []
   const number = new URL(request.url).searchParams.get('number')
-  if (identity) {
-    const identifier = identity.email
-    if (Number(number) > 0) {
-      let quota = await ctx.runQuery(internal.quotas.getUserQuota, {
-        identifier,
+  if (Number(number) > 0) {
+    const quota = await getUserQuota(identifier, ctx)
+    if (quota.queryLimit > 0) {
+      const recipes = await ctx.runAction(internal.spoonacular.complexSearch, {
+        number,
       })
-
-      console.log(new Date().getTime() - quota.last_reset_date)
-
-      if (
-        quota === null ||
-        new Date().getTime() - quota.last_reset_date > 1000 * 60 * 60 * 24
-      ) {
-        console.log('resetting quota')
-        const quotaID = await ctx.runMutation(internal.quotas.resetUserQuota, {
-          quotaID: quota?._id,
-          identifier,
-        })
-        quota = await ctx.runQuery(internal.quotas.getQuota, { quotaID })
-      }
-
-      if (quota.queryLimit > 0) {
-        console.log('able to query', quota.queryLimit)
-        const recipes = await ctx.runAction(
-          internal.spoonacular.complexSearch,
-          {
-            number,
-          },
-        )
-        results = recipes['results']
-        await ctx.runMutation(internal.quotas.updateQuota, {
-          quotaID: quota._id,
-          queryLimit: quota.queryLimit - 1,
-        })
-      } else {
-        console.log('quota limit reached')
-      }
-    }
-  } else {
-    if (Number(number) > 0 && Number(number) <= 4) {
-      const identifier = request.headers.get('x-forwarded-for') // ip address
-      let quota = await ctx.runQuery(internal.quotas.getUserQuota, {
-        identifier,
+      results = recipes['results']
+      await ctx.runMutation(internal.quotas.updateQuota, {
+        quotaID: quota._id,
+        queryLimit: quota.queryLimit - 1,
       })
-      if (
-        quota === null ||
-        new Date().getTime() - quota.last_reset_date > 1000 * 60 * 60 * 24
-      ) {
-        const quotaID = await ctx.runMutation(internal.quotas.resetUserQuota, {
-          identifier,
-        })
-        quota = await ctx.runQuery(internal.quotas.getQuota, { quotaID })
-      }
-      if (quota.queryLimit > 0) {
-        const recipes = await ctx.runAction(
-          internal.spoonacular.complexSearch,
-          {
-            number,
-          },
-        )
-        results = recipes['results']
-        await ctx.runMutation(internal.quotas.updateQuota, {
-          quotaID: quota._id,
-          queryLimit: quota.queryLimit - 1,
-        })
-      } else {
-        console.log('quota reached')
-      }
+    } else {
+      console.log('quota limit reached')
     }
   }
 
@@ -84,3 +33,22 @@ export const getRandom = httpAction(async (ctx, request) => {
     }),
   })
 })
+
+const getUserQuota = async (identifier, ctx) => {
+  let quota = await ctx.runQuery(internal.quotas.getUserQuota, {
+    identifier,
+  })
+
+  if (
+    quota === null ||
+    new Date().getTime() - quota.last_reset_date > 1000 * 60 * 60 * 24
+  ) {
+    console.log('resetting quota')
+    const quotaID = await ctx.runMutation(internal.quotas.resetUserQuota, {
+      quotaID: quota?._id,
+      identifier,
+    })
+    quota = await ctx.runQuery(internal.quotas.getQuota, { quotaID })
+  }
+  return quota
+}
