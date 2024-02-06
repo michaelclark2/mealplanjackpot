e0974f0d30817a337ad19a85eb43cf70337e12f5
Michael Clark <michael.clark1992@gmail.com>
2024-02-06 15:33:18 -0600

    started unauthed page view

--- packages/app/features/plans/list-screen.tsx
+++ packages/app/features/plans/list-screen.tsx
@@ -4,13 +4,15 @@ import { SignedIn, SignedOut } from '@clerk/clerk-react'
 
 export function MealPlanListScreen() {
   return (
-    <View className="flex-1">
-      <SignedIn>
-        <Text>Meal Plan List View</Text>
-      </SignedIn>
-      <SignedOut>
-        <Text>Create an account to start saving meal plans!</Text>
-      </SignedOut>
+    <View className="flex-1 p-4 sm:p-8">
+      <View className="min-h-[50%] rounded-3xl bg-slate-200 p-4">
+        <SignedIn>
+          <Text>Meal Plan List View</Text>
+        </SignedIn>
+        <SignedOut>
+          <Text>Create an account to start saving meal plans!</Text>
+        </SignedOut>
+      </View>
     </View>
   )
 }
