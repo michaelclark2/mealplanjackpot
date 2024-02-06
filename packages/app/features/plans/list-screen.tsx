8775397507bc7553b8319cd1dadf712293aa3b97
Michael Clark <michael.clark1992@gmail.com>
2024-02-06 16:01:55 -0600

    show unauthed users a login button

--- packages/app/features/plans/list-screen.tsx
+++ packages/app/features/plans/list-screen.tsx
@@ -1,8 +1,11 @@
 import { Text } from 'app/design/typography'
 import { View } from 'app/design/view'
 import { SignedIn, SignedOut } from '@clerk/clerk-react'
+import { Button } from 'app/design/button'
+import { useRouter } from 'solito/router'
 
 export function MealPlanListScreen() {
+  const router = useRouter()
   return (
     <View className="flex-1 p-4 sm:p-8">
       <View className="min-h-[50%] rounded-3xl bg-slate-200 p-4">
@@ -10,7 +13,14 @@ export function MealPlanListScreen() {
           <Text>Meal Plan List View</Text>
         </SignedIn>
         <SignedOut>
-          <Text>Create an account to start saving meal plans!</Text>
+          <View className="flex items-center space-y-10">
+            <Text className="text-center text-lg font-bold">
+              Create an account to view your saved meal plans!
+            </Text>
+            <Button onPress={() => router.push('/user/signin')}>
+              <Text>Login</Text>
+            </Button>
+          </View>
         </SignedOut>
       </View>
     </View>
