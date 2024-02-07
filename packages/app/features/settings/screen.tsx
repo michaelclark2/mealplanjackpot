7246240e2631a130726b28f53f0af79054d83814
Michael Clark <michael.clark1992@gmail.com>
2024-02-07 08:40:53 -0600

    added numberofrecipes form

--- packages/app/features/settings/screen.tsx
+++ packages/app/features/settings/screen.tsx
@@ -1,10 +1,14 @@
 import { Text } from 'app/design/typography'
 import { View } from 'app/design/view'
-import { Button } from 'app/design/button'
+import { Button, Pressable } from 'app/design/button'
 import { SignedIn, useAuth } from '@clerk/clerk-react'
 import { useRouter } from 'solito/router'
+import { useState } from 'react'
+import { Icon } from 'react-native-eva-icons'
+import colors from 'tailwindcss/colors'
 
 export function SettingsScreen() {
+  const [numberOfRecipes, setNumberOfRecipes] = useState<string>('4')
   const router = useRouter()
   const { signOut } = useAuth()
   const handleLogout = () => {
@@ -14,12 +18,44 @@ export function SettingsScreen() {
     <SignedIn>
       <View className="flex-1 p-4 md:p-8">
         {process.env.NEXT_PUBLIC_CONVEX_SITE ? (
-          <Text>Settings</Text>
-        ) : (
-          <Button onPress={handleLogout}>
+          <Text className="text-lg font-bold">Settings</Text>
+        ) : null}
+        <View className="m-auto w-full sm:w-1/2">
+          <View className="flex-row items-center justify-between">
+            <Text>Number of Recipes</Text>
+            <View className="w-1/2 flex-row items-center justify-between">
+              <Pressable
+                className="flex"
+                onPress={() =>
+                  setNumberOfRecipes((Number(numberOfRecipes) - 1).toString())
+                }
+              >
+                <Icon
+                  name="minus-square-outline"
+                  width={50}
+                  height={50}
+                  fill={colors.orange['500']}
+                />
+              </Pressable>
+              <Text className="text-3xl">{numberOfRecipes}</Text>
+              <Pressable
+                onPress={() =>
+                  setNumberOfRecipes((Number(numberOfRecipes) + 1).toString())
+                }
+              >
+                <Icon
+                  name="plus-square-outline"
+                  width={50}
+                  height={50}
+                  fill={colors.orange['500']}
+                />
+              </Pressable>
+            </View>
+          </View>
+          <Button className="m-auto" onPress={handleLogout}>
             <Text>Logout</Text>
           </Button>
-        )}
+        </View>
       </View>
     </SignedIn>
   )
